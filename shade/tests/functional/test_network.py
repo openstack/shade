9282eb2fdddb9126da8f34f9826ca5616268fd21
Tobias Brox <tobias@redpill-linpro.com>
2018-02-28 23:12:58 +0100

    Functional test robustness and performance fix
    In TestNetwork._cleanup_networks, pass network id instead of network name.
    Task: 6594
    Story: 2001617
    
    Change-Id: I696d6b9a8abf3e2d064bd276d1865452ef1e4fea

--- shade/tests/functional/test_network.py
+++ shade/tests/functional/test_network.py
@@ -34,7 +34,7 @@ class TestNetwork(base.BaseFunctionalTestCase):
         for network in self.operator_cloud.list_networks():
             if network['name'].startswith(self.network_name):
                 try:
-                    self.operator_cloud.delete_network(network['name'])
+                    self.operator_cloud.delete_network(network['id'])
                 except Exception as e:
                     exception_list.append(str(e))
                     continue
